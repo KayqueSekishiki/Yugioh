b6fa3486c00c0d600ea31502df96441465e3738b
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-11-09 01:22:37 -0300

    SCRIPT: Draw Cards!

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,11 @@ const state = {
   },
 };
 
+const playerSides = {
+  player1: "player-field-card",
+  computer: "computer-field-card",
+};
+
 const pathImages = ".src/assets/icons";
 const cardData = [
   {
@@ -46,6 +51,16 @@ const cardData = [
   },
 ];
 
+async function drawCards(cardNumbers, fieldSide) {
+  for (let i = 0; i < cardNumbers; i++) {
+    const randomIdCard = await getRandomCardId();
+    const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+    document.getElementById(fieldSide).appendChild(cardImage);
+  }
+}
+
 (function init() {
-  console.log(state);
+  drawCards(5, playerSides.player1);
+  drawCards(5, playerSides.computer);
 })();
