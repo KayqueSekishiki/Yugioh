17071305f6d427453b4af91055063cee7714f20e
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2023-11-09 23:57:43 -0300

    Add Audio!

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -192,6 +192,10 @@ function init() {
 
   drawCards(5, state.playerSides.player1);
   drawCards(5, state.playerSides.computer);
+
+  const bgm = document.getElementById("bgm");
+  bgm.play();
+  bgm.volume = 0.5;
 }
 
 init();
